4435b904eaca5cc79d7b2d162be162db5adf5f78
DavidMatias44 <daosmaru3667@gmail.com>
2025-03-26 07:37:18 -0600

    Avances en eventos.

--- resources/views/components/eventos/ultimos-eventos.blade.php
+++ resources/views/components/eventos/ultimos-eventos.blade.php
@@ -3,38 +3,57 @@
 		<h2>Últimos <br> eventos</h2>
 	</header>	
 
-	<div class="ContenedorUltimosEventos">
+	<div class="ultimos-eventos-cards">
+    	<!-- Tarjeta HCI -->
+		<div class="event-card card1">
+	       	<div class="title-event-card">HCI</div>
+        	<div class="text-event-card">
+				Descubra qué es la Interacción Humano-Computadora y en qué se centra el estudio
+        	</div>
+		</div>
+		
+    	<!-- Tarjeta CHI -->
+    	<div class="event-card card2">
+       		<div class="title-event-card">
+				Concurso de Diseño Estudiantil en CHI 2025
+			</div>
+    	</div>
+
+    	<!-- Tarjeta Ferias -->
+    	<div class="event-card card3">
+     		<div class="title-event-card">
+				Ferias tecnológicas
+			</div>
+    	</div>
+	</div>
+</div>
+
+	<!-- <div class="ContenedorUltimosEventos">
   		<div class="eventosContainer">
-    		<!-- Tarjeta HCI -->
     		<div id="event1-card" class="event-card">
       			<div id="event1" class="content-event-card">
-        		<!-- Titulo y contenido de la tarjeta -->
-        			<!-- <div class="title-event-card">HCI</div> -->
-        			<!-- <div class="text-event-card"> -->
-						<!-- Descubra qué es la Interacción Humano-Computadora y en qué se centra el estudio -->
-        			<!-- </div> -->
+        			<div class="title-event-card">HCI</div>
+        			<div class="text-event-card">
+						Descubra qué es la Interacción Humano-Computadora y en qué se centra el estudio
+        			</div>
       			</div>
     		</div>
 
-    	<!-- Tarjeta CHI -->
     	<div id="event2-card" class="event-card">
       	<div id="event2" class="content-event-card">
-        	<!-- Titulo de la tarjeta -->
-        	<!-- <div class="title-event-card"> -->
-				<!-- Concurso de Diseño Estudiantil en CHI 2025 -->
-			<!-- </div> -->
+        	<div class="title-event-card">
+				Concurso de Diseño Estudiantil en CHI 2025
+			</div>
       	</div>
     	</div>
 
-    <!-- Tarjeta Ferias -->
     <div id="event3-card" class="event-card">
       <div id="event3" class="content-event-card">
-        <!-- Titulo de la tarjeta -->
-        <!-- <div class="title-event-card"> -->
-			<!-- Ferias tecnológicas -->
-		<!-- </div> -->
+        <div class="title-event-card">
+			Ferias tecnológicas
+		</div>
       </div>
     </div>
   </div>
 </div>
-</div>
+</div> -->
